0d6d2738738a83879480116a3be18169a02d5cf8
GabrieleMaurina <gabrielemaurina95@gmail.com>
2022-04-05 10:58:17 -0600

    Imroved constructor and update

--- simpleobject.py
+++ simpleobject.py
@@ -4,8 +4,25 @@ class simpleobject(dict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for key, value in tuple(self.items()):
-            if issubclass(type(value), dict):
-                self[key] = simpleobject(value)
+            self[key] = self.__so__(value)
+
+    def update(self, other):
+        for key, value in other.items():
+            self[key] = self.__so__(value)
+
+    def __so__(self, value):
+        clazz = type(value)
+        if clazz == simpleobject:
+            return value
+        elif issubclass(clazz, dict):
+            return simpleobject(value)
+        elif issubclass(clazz, list):
+            return [self.__so__(v) for v in value]
+        elif issubclass(clazz, tuple):
+            return tuple(self.__so__(v) for v in value)
+        elif issubclass(clazz, set):
+            return {self.__so__(v) for v in value}
+        return value
 
     def __str__(self):
         name_key = '__name__' if '__name__' in self else 'name' if 'name' in self else None
